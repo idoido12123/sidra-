ec52307f9feb74f61e28673cd448709017d4673a
idoido12123 <ak6361@bs.amalnet.k12.il>
2018-10-07 11:09:19 +0300

    1st

---
+++ app/src/main/java/com/example/idoid/sidra/Main2Activity.java
@@ -0,0 +1,72 @@
+package com.example.idoid.sidra;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    Intent get = getIntent();
+    double num1, d;
+    int typeprog;
+    ListView lv1;
+    TextView tv1, tv2, tv3, tv4;
+    Double[] numbers=new Double[20];
+    String [] numbers2=new String[20];
+    Double schum = 0.0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        typeprog = get.getIntExtra("typeProg", 0);
+        num1 = get.getDoubleExtra("firstNum", 0);
+        d = get.getDoubleExtra("d", 0);
+        tv1=(TextView)findViewById(R.id.textView7);
+        tv2 = (TextView) findViewById(R.id.textView8);
+        tv3 = (TextView) findViewById(R.id.textView9);
+        tv4 = (TextView) findViewById(R.id.textView10);
+        lv1 = (ListView) findViewById(R.id.listview);
+        numbers[0] = num1;
+        numbers2[0]=""+num1;
+        if (typeprog == 1) {
+           for (int i = 1; i < 20; i++) {
+                numbers[i] = d + numbers[i - 1];
+                numbers2[i]=""+numbers[i];
+
+
+            }
+        } else {
+            for (int i = 1; i < 20; i++) {
+               numbers[i] = d * numbers[i - 1];
+                numbers2[i]=""+numbers[i];
+            }
+        }
+        lv1.setOnItemClickListener(this);
+        lv1.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        ArrayAdapter<String> adp = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, numbers2);
+          lv1.setAdapter(adp);
+    }
+
+   @Override
+   public void onItemClick(AdapterView<?> AdapterView, View view, int position, long id) {
+       for (int i = 0; i < position; i++) {
+        schum = schum + numbers[i];
+      }
+
+         tv1.setText("" + num1);
+          tv2.setText("" + d);
+         tv3.setText("" + position);
+           tv4.setText("" + schum);
+
+       }
+    }
+
+
---
+++ app/src/main/java/com/example/idoid/sidra/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.idoid.sidra;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+    RadioButton rb1,rb2;
+    Button btn1;
+    EditText et1,et2;
+    Intent t1;
+    double num1,d;
+    String st;
+    int typeProg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button) findViewById(R.id.button);
+        et1 = (EditText) findViewById(R.id.firstNum);
+        et2 = (EditText) findViewById(R.id.MultOrDif);
+        rb1 = (RadioButton) findViewById(R.id.aritProg);
+        rb2 = (RadioButton) findViewById(R.id.geoProg);
+    }
+
+    public void go(View view) {
+        t1 = new Intent(this, Main2Activity.class);
+        st = et1.getText().toString();
+        num1 = Double.parseDouble(st);
+        st = et2.getText().toString();
+        d = Double.parseDouble(st);
+        if (rb1.isChecked()) typeProg=1;
+        if (rb2.isChecked()) typeProg=2;
+        t1.putExtra("typeProg",typeProg);
+        t1.putExtra("firstNum",num1);
+        t1.putExtra("d",d);
+        startActivity(t1);
+    }
+}
